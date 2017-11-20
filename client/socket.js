493492e0aa9684f212cdc289efd9f7fe5c240905
EHenry92 <EHenry692@hotmail.com>
2017-11-20 14:12:53 -0500

    added sockets for new notes

--- client/socket.js
+++ client/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
-import store, { postMessage } from './store';
+import store, { postMessage, addNote, editNote, deleteNote } from './store';
 
 const socket = io(window.location.origin)
 
@@ -16,5 +16,20 @@ socket.on('new-message', message => {
   store.dispatch(postMessage(message));
   console.log('after dispatch')
 });
+socket.on('new-note', note => {
+  console.log('A new note has bee posted', note)
+  store.dispatch(addNote(note));
+  console.log('new note has been posted');
+})
+socket.on('edit-note', (id, data) => {
+  console.log('A note has been edited', data)
+  store.dispatch(editNote(id, data));
+  console.log('Edits have been reflected')
+})
+socket.on('delete-note', id =>  {
+  console.log('A note has been deleted')
+  store.dispatch(deleteNote(id))
+  console.log('Note Removed')
+})
 
 export default socket
--- client/store/notes.js
+++ client/store/notes.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import socket from '../socket';
 import { addNoteToBoard } from './whiteboard'
 
 const GET_NOTES = 'GET_NOTES'
@@ -26,6 +27,7 @@ export const addNote = (note) =>
     axios.post(`/api/notes`, { note })
       .then(createdNote => {
         dispatch(insertNote(createdNote.data))
+        socket.emit('new-note', note)
       })
       .catch(err => console.log(err))
     }
@@ -33,7 +35,10 @@ export const addNote = (note) =>
 export const deleteNote = note =>
   dispatch =>
     axios.delete(`/api/notes/${note.id}`)
-      .then(_ => dispatch(removeNote(note.id)))
+      .then(_ => {
+        dispatch(removeNote(note.id))
+        socket.emit('delete-note', note.id)
+      })
       .catch(err => console.log(err))
 
 export const editNote = (id, data) =>
@@ -41,6 +46,7 @@ export const editNote = (id, data) =>
     axios.put(`/api/notes/${id}`, data)
       .then(updatedNote => {
         dispatch(updateNote(updatedNote.data))
+        socket.emit('edit-note', id, data)
       })
       .catch(err => console.log(err))
 
