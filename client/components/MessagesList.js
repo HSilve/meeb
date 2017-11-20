434f911a935cd8d78eb6242bd7b3db9bc4995241
Maria Betances <mbetances1002@gmail.com>
2017-11-20 15:46:54 -0500

    chatbox filters messages based on whiteboard Id

--- client/components/MessagesList.js
+++ client/components/MessagesList.js
@@ -5,22 +5,22 @@ import { withRouter } from 'react-router'
 
 export class MessagesList extends Component {
   componentDidMount(){
-    const { id } = this.props.match.params
-    this.props.getMessages(id)
+    this.props.getMessages()
   }
 
   render () {
-    const {messageEntry} = this.props
-    console.log('allMessages', messageEntry.allMessages)
+    const { messageEntry } = this.props
     return (
       <div id="live-chat">
         {
-          messageEntry.allMessages &&  messageEntry.allMessages.map(message => {
-            console.log('inside .map', message.user)
+          messageEntry.allMessages &&  messageEntry.allMessages.filter(message =>
+            message.whiteboard.id === Number(this.props.match.params.id)
+          ).map(message => {
             return (<div className="blob" id="chat-message" key={message.id}>
-              { message.user && <span className="chip">
-                  <img src={message.user.image} /></span> }
-                <span><b>{message.user.name}</b></span>
+              <span className="chip">
+                <img src={message.user.image} />
+              </span>
+              <span><b>{message.user.name}</b></span>
               <div>{message.text}</div>
             </div>)
           })
@@ -33,8 +33,8 @@ export class MessagesList extends Component {
 const mapState = ({whiteboard, messageEntry}) => ({whiteboard, messageEntry})
 
 const mapDispatch = (dispatch) => ({
-  getMessages: (id) => {
-    dispatch(fetchMessages(id))
+  getMessages: () => {
+    dispatch(fetchMessages())
   }
 })
 
