3df13b4a176c2d1316aa8a930973db17c5f96d1c
echai13 <cher13@brandeis.edu>
2017-11-20 15:27:14 -0500

    dragging part 1

--- client/components/Whiteboard.js
+++ client/components/Whiteboard.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchRoom, editNote, fetchNotes } from '../store'
-import history from 'history'
 
 export class Whiteboard extends Component {
   constructor (props) {
     super(props)
+    console.log(this.props)
+    this.state = {
+      dragging: false,
+      rel: null,
+      pos: {x: 0, y: 0},
+      selectedNote: 0
+    }
+
     this.positions = []
     this.clickImage = this.clickImage.bind(this);
     this.removePosition = this.removePosition.bind(this);
-
+    this.onMouseDown = this.onMouseDown.bind(this)
+    this.onMouseUp = this.onMouseUp.bind(this)
+    this.onMouseMove = this.onMouseMove.bind(this)
   }
 
 //In getBoundingClientREact:
@@ -28,75 +37,109 @@ export class Whiteboard extends Component {
 
     // Returns a random integer between min (included) and max (excluded)
     // Using Math.round() will give you a non-uniform distribution!
-    getRandomInt(min, max) {
-      return Math.floor(Math.random() * (max - min)) + min;
-    }
+  getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-    // generate random positions
-    generatePositionsArray(boardHeight, boardWidth, safeHeight, safeWidth, leftBegin, topBegin) {
-      let irregularity = 0.5;
-        // declarations
-        var positionsArray = [];
-        var r, c;
-        var rows;
-        var columns;
-        // count the amount of rows and columns
-        rows = Math.floor(boardHeight / safeHeight);
-        columns = Math.floor(boardWidth / safeWidth);
-        // loop through rows
-        for (r = 1; r <= rows; r += 1) {
-            // loop through columns
-            for (c = 1; c <= columns; c += 1) {
-                // populate array with point object
-                positionsArray.push({
-                    x: Math.round(boardWidth * c / columns) + (this.getRandomInt(irregularity * -1, irregularity)) + (leftBegin / 4),
-                    y: Math.round(boardHeight * r / rows) + (this.getRandomInt(irregularity * -1, irregularity))
-                });
-            }
-        }
-        // return array
-        return positionsArray;
-    }
-    // get random position from positions array
-    getRandomPosition(removeTaken) {
-        // declarations
-        var randomIndex;
-        var coordinates;
-        // get random index
-        randomIndex = this.getRandomInt(0, this.positions.length - 1);
-        // get random item from array
-        coordinates = this.positions[randomIndex];
-        // check if remove taken
-        if (removeTaken) {
-            // remove element from array
-            this.positions = [...this.positions.slice(0, randomIndex), ...this.positions.slice(randomIndex + 1)]
-        }
-        // return position
-        return coordinates;
-    }
-    // getRandomPosition(positions, true)
-    getPosition(noteId) {
-      let pos = this.getRandomPosition(true);
-      this.props.editNote(noteId, {position: [pos.x, pos.y]})
-      return {
-        style: {
-          position: 'fixed',
-          left: pos.x,    // computed based on child and parent's height
-          top: pos.y   // computed based on child and parent's width
-        }
+  // generate random positions
+  generatePositionsArray(boardHeight, boardWidth, safeHeight, safeWidth, leftBegin, topBegin) {
+    let irregularity = 0.5;
+      // declarations
+      var positionsArray = [];
+      var r, c;
+      var rows;
+      var columns;
+      // count the amount of rows and columns
+      rows = Math.floor(boardHeight / safeHeight);
+      columns = Math.floor(boardWidth / safeWidth);
+      // loop through rows
+      for (r = 1; r <= rows; r += 1) {
+          // loop through columns
+          for (c = 1; c <= columns; c += 1) {
+              // populate array with point object
+              positionsArray.push({
+                  x: Math.round(boardWidth * c / columns) + (this.getRandomInt(irregularity * -1, irregularity)) + (leftBegin / 4),
+                  y: Math.round(boardHeight * r / rows) + (this.getRandomInt(irregularity * -1, irregularity))
+              });
+          }
+      }
+      // return array
+      return positionsArray;
+  }
+  // get random position from positions array
+  getRandomPosition(removeTaken) {
+      // declarations
+      var randomIndex;
+      var coordinates;
+      // get random index
+      randomIndex = this.getRandomInt(0, this.positions.length - 1);
+      // get random item from array
+      coordinates = this.positions[randomIndex];
+      // check if remove taken
+      if (removeTaken) {
+          // remove element from array
+          this.positions = [...this.positions.slice(0, randomIndex), ...this.positions.slice(randomIndex + 1)]
+      }
+      // return position
+      return coordinates;
+  }
+  // getRandomPosition(positions, true)
+  getPosition(noteId) {
+    let pos = this.getRandomPosition(true);
+    this.props.editNote(noteId, {position: [pos.x, pos.y]})
+    return {
+      style: {
+        position: 'fixed',
+        left: pos.x,    // computed based on child and parent's height
+        top: pos.y   // computed based on child and parent's width
       }
     }
+  }
 
-    removePosition(takenPosition) {
-      this.positions = this.positions.filter(aPosition =>
-        (aPosition[0] != takenPosition[0]) && (aPosition[1] != takenPosition[1])
-      )
-    }
+  removePosition(takenPosition) {
+    this.positions = this.positions.filter(aPosition =>
+      (aPosition[0] != takenPosition[0]) && (aPosition[1] != takenPosition[1])
+    )
+  }
 
-    clickImage (evt) {
-      evt.preventDefault();
+  clickImage (evt) {
+    evt.preventDefault();
+  }
 
-    }
+  onMouseDown(evt) {
+    if (evt.button !== 0) return
+    var pos = this.notes[this.state.selectedNote].getBoundingClientRect()
+    console.log(this.notes[this.state.selectedNote].getBoundingClientRect())
+    this.setState({
+      dragging: true,
+      rel: {
+        x: evt.pageX - pos.left,
+        y: evt.pageY - pos.top
+      }
+    })
+    evt.stopPropagation()
+    evt.preventDefault()
+  }
+
+  onMouseUp(evt) {
+    this.setState({dragging: false})
+    evt.stopPropagation()
+    evt.preventDefault()
+  }
+
+  onMouseMove(evt) {
+    console.log('enter')
+    if (!this.state.dragging) return
+    this.setState({
+      pos: {
+        x: evt.pageX - this.state.rel.x,
+        y: evt.pageY - this.state.rel.y
+      }
+    })
+    console.log(this.state.pos)
+    evt.stopPropagation()
+    evt.preventDefault()
+  }
 
 
   render() {
@@ -115,7 +158,13 @@ export class Whiteboard extends Component {
           {
             return   note.position ?
              (
-                  <div className="card" key={note.id} style = {{position: 'absolute', left: note.position[0], top: note.position[1] }} >
+                  <div className="card"
+                    key={note.id}
+                    style = {{position: 'absolute', left: note.position[0], top: note.position[1], cursor: 'pointer' }}
+                    onMouseMove={this.onMouseMove}
+                    onMouseUp={this.onMouseUp}
+                    onMouseDown={this.onMouseDown}
+                    ref={ref => this.notes[note.id] = ref} >
 
                     { note.text &&
                       <div className="card-content">
