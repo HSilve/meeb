2e55f28117172124a39d1fa605a62ac00fb30199
Maria Betances <mbetances1002@gmail.com>
2017-11-21 22:48:06 -0500

    minimize button for sidebar

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -6,21 +6,34 @@ import Attendees from './Attendees'
 import { fetchRoom } from '../store'
 
 export class Sidebar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: true
+    }
+  }
   componentDidMount() {
     const { id } = this.props.match.params
     this.props.getWhiteboard(id)
   }
 
   render() {
+    const { show } = this.state
     return (
       <div className="sidebar">
-        <h5 href="#">
-          <div>Attendees</div>
-          <i alt="Brand" className="glyphicon glyphicon-comment">
-          </i>
-        </h5>
-        <Attendees />
-        <Chatbox />
+        <button onClick={() => this.setState({ show: !show })}>-</button>
+          {
+            show ?
+            <div>
+              <h5 href="#">
+              <div>Attendees</div>
+              <i alt="Brand" className="glyphicon glyphicon-comment">
+              </i>
+            </h5>
+            <Attendees />
+            <Chatbox />
+            </div> : null
+          }
       </div>
     );
   }
