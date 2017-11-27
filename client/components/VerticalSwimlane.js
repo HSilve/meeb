50a09019c99ecaae228566ee505e04fbaa84489b
Blanca Sanchez <blancayllen@gmail.com>
2017-11-27 16:07:12 -0500

    merge conflict

---
+++ client/components/VerticalSwimlane.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router'
+import { connect } from 'react-redux'
+
+class VerticalSwimlane extends Component{
+  render(){
+    return(
+      <div>
+        <div className="row">
+          <div className="col s1"></div>
+          <div className="col s1"></div>
+          <div className="col s1"></div>
+          <div className="col s1"></div>
+          <div className="col s1"></div>
+          <div className="col s1"></div>
+          <div className="col s1"></div>
+          <div className="col s1"></div>
+          <div className="col s1"></div>
+          <div className="col s1"></div>
+          <div className="col s1"></div>
+          <div className="col s12">
+            <svg className="swimlaneLine">
+            </svg>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapState = null
+const mapDispatch = null
+
+export default withRouter(connect(mapState, mapDispatch)(VerticalSwimlane))
