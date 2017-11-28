4ded98e675fed8ba9e2d3103094c2c2b9847a0af
EHenry92 <EHenry692@hotmail.com>
2017-11-28 17:08:12 -0500

    action panel clears on input

--- client/components/ActionPanel.js
+++ client/components/ActionPanel.js
@@ -93,7 +93,12 @@ class ActionPanel extends React.Component {
                   }
                 </li>
               </ul>
-              <form id="actionForm" onSubmit={(evt) => { evt.preventDefault(); this.props.handleSubmit(evt, this.state.file, this.state.name, this.state.type, this.props.user.id, this.props.match.params.id, this.props.notes.length) }} style={{ bottom: '90px', right: '100px', position: 'fixed' }}>
+              <form
+                    id="actionForm"
+                    onSubmit={(evt) => { evt.preventDefault();
+                        this.props.handleSubmit(evt, this.state.file, this.state.name, this.state.type, this.props.user.id, this.props.match.params.id, this.props.notes.length);
+                        this.setState({ expandToggle: false, textToggle: false, imageToggle: false, linkToggle: false }) }}
+                      style={{ bottom: '90px', right: '100px', position: 'fixed' }}>
                 {(this.state.textToggle) && <div><input name="text" type="text" /><button type="submit">Insert</button></div>}
                 {(this.state.linkToggle) && <div><input name="link" type="text" /><button type="submit">Insert</button></div>}
                 {this.state.imageToggle &&
