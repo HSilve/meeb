d765a84c33e18fb5e34c45ca0e266dad3c0b0983
echai13 <cher13@brandeis.edu>
2017-12-04 23:48:01 -0500

    notes specs

---
+++ client/components/ActionPanel.spec.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { expect } from 'chai'
+import enzyme, { shallow, mount } from 'enzyme'
+import { ActionPanel, ConferenceRoom } from './index'
+import Adapter from 'enzyme-adapter-react-16'
+enzyme.configure({ adapter: new Adapter() })
+// var jsdom = require('jsdom')
+// const { JSDOM } = jsdom;
+// const { document } = (new JSDOM('')).window;
+// global.document = document;
+
+
+describe('Frontend tests', () => {
+  describe('ActionPanel component', () => {
+    describe('toggle method', () => {
+      let wrapper = shallow(<ActionPanel />)
+
+    })
+  })
+})
